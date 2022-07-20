fe75eab1b3ce594ff1e66c18da1ea74cdaaf815f
mr-himanshjain <hj10himanshu@gmail.com>
2022-07-21 01:44:44 +0530

    update on function

--- functions2.py
+++ functions2.py
@@ -6,23 +6,23 @@ def start(x):
     return
 print("hello")
 start(10)
-
+###########################################
 def second(a,b):
     print(a,b)
     return
 a=40
 second(a,55)
-
+#######################################
 def second(q,ty):
     print(q,ty)
     return
 second(34,ty=55)
-
+#########################################
 def second(o,*t):
     print(o,t)
     return
 second(23,55,54,66,23)
-
+##########################################
 def second(o,*t):
     print(o,t)
     for i in t:
